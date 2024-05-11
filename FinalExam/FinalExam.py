abe6cdbf1a3d20a2e287d3e88e9e8e957c13cd1d
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-11 23:53:11 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -34,9 +34,14 @@ def import_and_predict(image_data,model):
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
+    image = Image.open(file)
     st.image(image,use_column_width=True)
-    prediction = import_and_predict(image, model)
+    size=(64,64)
+    image = ImageOps.fit(image,size, PIL.Image.Resampling.LANCZOS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
     class_names=['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Shoe']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
