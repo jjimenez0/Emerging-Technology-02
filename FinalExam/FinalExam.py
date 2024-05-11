cba8dcdb6f917abf12077826cc66b86c3aee8e6f
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-12 01:03:09 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -38,13 +38,18 @@ def load_image(image):
 	img = img.astype('float32')
 	img = img / 255.0
 	return img
-    
+
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image)
-    load_image(image)
+    img = img_to_array(image)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(3, 28, 28, 1)
+    # prepare pixel data
+    img = img.astype('float32')
+    img = img / 255.0
     prediction = numpy.argmax(model.predict(img), axis=1)
     class_names=['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Shoe']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
