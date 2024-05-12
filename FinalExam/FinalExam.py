a11a112473b3e87f06e8c7abb5a3dd4468f5a397
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-12 12:03:46 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -14,21 +14,28 @@ file=st.file_uploader("Choose a Fashion Outfit from computer",type=["jpg","png"]
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
-def import_and_predict(image_data,model):
-    size=(28,28)
-    image=ImageOps.fit(image_data,size)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+def load_image(filename):
+  	# load the image
+  	img = load_img(filename, target_size=(28, 28))
+  	# convert to array
+  	img = img_to_array(img)
+  	# reshape into a single sample with 3 channels
+  	img = img.reshape(3, 28, 28, 1)
+  	# prepare pixel data
+  	img = img.astype('float32')
+    img = img / 255.0
+  	return img
 
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
+    img = load_image(file)
+    st.image(img,use_column_width=True)
+    result = numpy.argmax(model.predict(img), axis=1)
+    return result
     class_names=['T-shirt', 'Trouser', 'Pullover', 'Dress','Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Shoe']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
