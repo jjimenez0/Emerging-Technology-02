1212f2398ea3f5ebab7c5aad219e548992dc019f
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-12 12:40:48 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -22,7 +22,7 @@ def import_and_predict(image_data,model):
     image=ImageOps.fit(image_data,size)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    prediction=model.predict_classes(img_reshape)
     return prediction
 
 if file is None:
