fb08158ab0effc57bab638b66dfd815b044efcaa
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-16 14:04:47 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 
-@st.cache_resource
+@st.cache(allow_output_mutation=True)
 def load_model():
   model=tf.keras.models.load_model('final_model1.h5')
   return model
