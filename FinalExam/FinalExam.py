fd086812a43cf127d2e879259036cb37f5c1ec05
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-05-16 14:01:05 +0800

    Update FinalExam.py

--- FinalExam/FinalExam.py
+++ FinalExam/FinalExam.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=('final_model1.hdf5')
+  model=tf.keras.models.load_model('final_model1.hdf5')
   return model
 model = load_model()
 st.write("""
